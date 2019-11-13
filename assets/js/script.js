7e0684f11f6341b93d8bec04569519a56233c138
Anastasia Canlas <recr38@gmail.com>
2019-11-13 09:46:26 -0800

    Completed flipping one card - pending staff review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+$(document).ready(initializeApp);
 
+function initializeApp() {
+  $('.card').on('click', handleCardClick);
+}
+
+function handleCardClick(event){
+  var selectedCard = event.currentTarget;
+  var selectedCardBack = $(selectedCard).find('.back');
+  var selectedCardFront = $(selectedCard).find('.front');
+  selectedCardBack.addClass('hidden');
+  selectedCardFront.addClass('js-logo');
+  // console.log(selectedCardBack);
+}
