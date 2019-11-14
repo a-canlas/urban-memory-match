5547fe48281200bc27cc3c2da30883756864839f
Anastasia Canlas <recr38@gmail.com>
2019-11-13 16:25:04 -0800

    Completed Flip Two Cards feature set

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,37 @@ function initializeApp() {
   $('.card').on('click', handleCardClick);
 }
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
 function handleCardClick(event){
-  var selectedCard = event.currentTarget;
-  var selectedCardBack = $(selectedCard).find('.back');
-  var selectedCardFront = $(selectedCard).find('.front');
-  selectedCardBack.addClass('hidden');
-  selectedCardFront.addClass('js-logo');
-  // console.log(selectedCardBack);
+  if(!firstCardClicked){
+    firstCardClicked = $(event.currentTarget);
+    firstCardClicked.find('.back').addClass('hidden');
+  } else {
+    secondCardClicked = $(event.currentTarget);
+    secondCardClicked.find('.back').addClass('hidden');
+  }
+  if(firstCardClicked && secondCardClicked){
+    var cssValue1 = firstCardClicked.find('.front').css("background-image");
+    var cssValue2 = secondCardClicked.find('.front').css("background-image");
+    if(cssValue1 === cssValue2){
+      console.log('cards match');
+      matches ++;
+      console.log('Number of matches: ', matches);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else{
+      setTimeout(function(){
+        firstCardClicked.find('.back').removeClass('hidden');
+        secondCardClicked.find('.back').removeClass('hidden');
+        firstCardClicked = null;
+        secondCardClicked = null;
+
+      }, 1500);
+    }
+
+  }
+
 }
